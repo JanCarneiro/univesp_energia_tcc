7458228961cf4a19bf83045de0c0edb7a1275044
JanCarneiro <jan-porsche@hotmail.com>
2025-03-05 20:40:19 -0300

    adicao_conn_api

--- dependencias/conn_spark.py
+++ dependencias/conn_spark.py
@@ -3,19 +3,14 @@ from pyspark.sql import SparkSession
 
 findspark.init("/home/jan/spark/spark-3.5.5-bin-hadoop3")
 
-class CriarConn():
-    def __init__(self, name_app):
-
-        self.name_app = name_app
-
-    def cria_conn(self):
-        
-        self.spark = SparkSession.builder \
-            .appName(self.name_app) \
-            .config("spark.driver.memory", "16g") \
-            .config("spark.executor.memory", "16g") \
-            .master("local[*]") \
-            .getOrCreate()
-        
-        #display(self.spark)
-        return self.spark
+def cria_conn(name_app):
+    
+    spark = SparkSession.builder \
+        .appName(name_app) \
+        .config("spark.driver.memory", "16g") \
+        .config("spark.executor.memory", "16g") \
+        .master("local[*]") \
+        .getOrCreate()
+    
+    #display(self.spark)
+    return spark
